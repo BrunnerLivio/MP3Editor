a55b94373050c844f4d046af3514f2ae77baeb47
brunnerlivio <livio.brunner@hotmail.com>
2016-12-02 13:41:27 +0100

    finish id3v1 test

--- MP3Editor.Businesslogic.Test/ID3V1.cs
+++ MP3Editor.Businesslogic.Test/ID3V1.cs
@@ -8,15 +8,47 @@ namespace MP3Editor.Businesslogic.Test
     [TestClass]
     public class Id3V1
     {
-        [TestMethod]
-        public void ReadFile()
+        private string filepath = "Give Me The Night.mp3";
+        private string album = "George Benson Best Hits";
+        private string comment = "My comment xD";
+        private string genre = "Pop";
+        private int year = 1981;
+        private string title = "Give Me The Night";
+        private int track = 1;
+        private Mock<IID3V1> CreateID3V1File()
         {
-            
-            string filepath = "Give Me The Night.mp3";
-            var reader = new Mock<IFileTypeReader>();
             var id3v1file = new Mock<IID3V1>();
             id3v1file.Setup(i => i.Filename)
                 .Returns(filepath);
+
+            id3v1file.Setup(i => i.Album)
+                .Returns(album);
+
+            id3v1file.Setup(i => i.Comment)
+                .Returns(comment);
+
+            id3v1file.Setup(i => i.Genre)
+                .Returns(genre);
+
+            id3v1file.Setup(i => i.Year)
+                .Returns(year);
+
+            id3v1file.Setup(i => i.Track)
+                .Returns(track);
+
+            id3v1file.Setup(i => i.Title)
+                .Returns(title);
+
+            return id3v1file;
+        }
+        [TestMethod]
+        public void ReadFile()
+        {
+            Mock<IID3V1> id3v1file = CreateID3V1File();
+            
+
+            var reader = new Mock<IFileTypeReader>();
+            
 
             reader
                 .Setup(r => r.Read(filepath))
@@ -24,6 +56,12 @@ namespace MP3Editor.Businesslogic.Test
 
             IID3V1 file = (IID3V1)reader.Object.Read(filepath);
             Assert.AreEqual(file.Filename, filepath);
+            Assert.AreEqual(file.Album, album);
+            Assert.AreEqual(file.Comment, comment);
+            Assert.AreEqual(file.Genre, genre);
+            Assert.AreEqual(file.Track, track);
+            Assert.AreEqual(file.Title, title);
+            Assert.AreEqual(file.Year, year);
         }
 
         [TestMethod]
